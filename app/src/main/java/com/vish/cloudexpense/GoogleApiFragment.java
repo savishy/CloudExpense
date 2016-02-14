e3de0b897821372082ddb43d0260177ac173fe66
vish <savishy.pv@gmail.com>
2016-02-14 15:54:06 +0530

    WIP. all previous Google code has been moved to Fragment. The good thing it's got going for it is that it does not crash :)

--- app/src/main/java/com/vish/cloudexpense/GoogleApiFragment.java
+++ app/src/main/java/com/vish/cloudexpense/GoogleApiFragment.java
@@ -1,34 +1,88 @@
 package com.vish.cloudexpense;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.script.model.ExecutionRequest;
+import com.google.api.services.script.model.Operation;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link GoogleApiFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link GoogleApiFragment#newInstance} factory method to
- * create an instance of this fragment.
  *
  * References:
  * http://stackoverflow.com/a/28849764/682912
  */
 public class GoogleApiFragment extends Fragment {
 
+    private static final String TAG = "GoogleApiFragment";
+    GoogleAccountCredential mCredential;
+    private TextView mOutputText;
+    private ListView expenseCategories;
+    private ArrayAdapter<String> arrayAdapter;
+    private List<String> arrayList = new ArrayList<String>();
+    ProgressDialog mProgress;
 
+    static final int REQUEST_ACCOUNT_PICKER = 1000;
+    static final int REQUEST_AUTHORIZATION = 1001;
+    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    private static final String PREF_ACCOUNT_NAME = "accountName";
+
+    //This is the list of OAuth scopes required by the
+    //Google Apps script.
+    private static final String[] SCOPES = {
+            "https://www.googleapis.com/auth/drive",
+            "https://www.googleapis.com/auth/spreadsheets"
+    };
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //TODO why am I doing this?
-        setRetainInstance(true);
+
+        mProgress = new ProgressDialog(getActivity());
+        mProgress.setMessage("Calling Google Apps Script Execution API ...");
+
+
+        // Initialize credentials and service object.
+        SharedPreferences settings = getActivity().getPreferences(Context.MODE_PRIVATE);
+        mCredential = GoogleAccountCredential.usingOAuth2(
+                getActivity().getApplicationContext(), Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff())
+                .setSelectedAccountName(settings.getString(PREF_ACCOUNT_NAME, null));
     }
 
     @Nullable
@@ -40,7 +94,279 @@ public class GoogleApiFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        Common.showSimpleDialog(getActivity(),"onActivityCreated","Activity has been created");
+        Common.showSimpleDialog(getActivity(), "onActivityCreated", "Activity has been created");
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (isGooglePlayServicesAvailable()) {
+            refreshResults();
+        } else {
+            mOutputText.setText("Google Play Services required: " +
+                    "after installing, close and relaunch this app.");
+        }
+    }
+
+    /**
+     * Extend the given HttpRequestInitializer (usually a credentials object)
+     * with additional initialize() instructions.
+     *
+     * @param requestInitializer the initializer to copy and adjust; typically
+     *         a credential object.
+     * @return an initializer with an extended read timeout.
+     */
+    private static HttpRequestInitializer setHttpTimeout(
+            final HttpRequestInitializer requestInitializer) {
+        return new HttpRequestInitializer() {
+            @Override
+            public void initialize(HttpRequest httpRequest)
+                    throws IOException {
+                requestInitializer.initialize(httpRequest);
+                // This allows the API to call (and avoid timing out on)
+                // functions that take up to 6 minutes to complete (the maximum
+                // allowed script run time), plus a little overhead.
+                httpRequest.setReadTimeout(380000);
+            }
+        };
+    }
+
+    /**
+     * Check that Google Play services APK is installed and up to date. Will
+     * launch an error dialog for the user to update Google Play Services if
+     * possible.
+     * @return true if Google Play Services is available and up to
+     *     date on this device; false otherwise.
+     */
+    private boolean isGooglePlayServicesAvailable() {
+        final int connectionStatusCode =
+                GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity());
+        if (GooglePlayServicesUtil.isUserRecoverableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+            return false;
+        } else if (connectionStatusCode != ConnectionResult.SUCCESS ) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Display an error dialog showing that Google Play Services is missing
+     * or out of date.
+     * @param connectionStatusCode code describing the presence (or lack of)
+     *     Google Play Services on this device.
+     */
+    void showGooglePlayServicesAvailabilityErrorDialog(
+            final int connectionStatusCode) {
+        Dialog dialog = GooglePlayServicesUtil.getErrorDialog(
+                connectionStatusCode,
+                getActivity(),
+                REQUEST_GOOGLE_PLAY_SERVICES);
+        dialog.show();
+    }
+
+    /**
+     * Starts an activity in Google Play Services so the user can pick an
+     * account.
+     */
+    private void chooseAccount() {
+        startActivityForResult(
+                mCredential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+    }
+
+    /**
+     * Attempt to get a set of data from the Google Apps Script Execution API to display. If the
+     * email address isn't known yet, then call chooseAccount() method so the
+     * user can pick an account.
+     */
+    private void refreshResults() {
+        if (mCredential.getSelectedAccountName() == null) {
+            chooseAccount();
+        } else {
+            if (isDeviceOnline()) {
+                new MakeRequestTask(mCredential).execute();
+            } else {
+                mOutputText.setText("No network connection available.");
+            }
+        }
+    }
+
+
+
+    /**
+     * Checks whether the device currently has a network connection.
+     * @return true if the device has a network connection, false otherwise.
+     */
+    private boolean isDeviceOnline() {
+        ConnectivityManager connMgr =
+                (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    /**
+     * An asynchronous task that handles the Google Apps Script Execution API call.
+     * Placing the API calls in their own task ensures the UI stays responsive.
+     */
+    private class MakeRequestTask extends AsyncTask<Void, Void, List<String>> {
+        private com.google.api.services.script.Script mService = null;
+        private Exception mLastError = null;
+
+        public MakeRequestTask(GoogleAccountCredential credential) {
+            HttpTransport transport = AndroidHttp.newCompatibleTransport();
+            JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+            mService = new com.google.api.services.script.Script.Builder(
+                    transport, jsonFactory, setHttpTimeout(credential))
+                    .setApplicationName("CloudExpenseTracker")
+                    .build();
+        }
+
+        /**
+         * Background task to call Google Apps Script Execution API.
+         * @param params no parameters needed for this task.
+         */
+        @Override
+        protected List<String> doInBackground(Void... params) {
+            try {
+                return getDataFromApi();
+            } catch (Exception e) {
+                mLastError = e;
+                cancel(true);
+                return null;
+            }
+        }
+
+        /**
+         * Call the API to run an Apps Script function that returns a list
+         * of categorized expenses.
+         *
+         * @return list of String folder names and their IDs
+         * @throws IOException
+         */
+        private List<String> getDataFromApi()
+                throws IOException, GoogleAuthException {
+            // ID of the script to call. Acquire this from the Apps Script editor,
+            // under Publish > Deploy as API executable.
+            String scriptId = "MM4uLUw9bV6-vUZuxqHcH-XvYgb_i6vya";
+
+            List<String> folderList = new ArrayList<String>();
+
+            // Create an execution request object.
+            //Here we will set the Google Apps Script function to execute.
+            ExecutionRequest request = new ExecutionRequest()
+                    .setFunction("getExpensesByCategory");
+
+            // Make the request.
+            Operation op =
+                    mService.scripts().run(scriptId, request).execute();
+
+            // Print results of request.
+            if (op.getError() != null) {
+                throw new IOException(getScriptError(op));
+            }
+            if (op.getResponse() != null &&
+                    op.getResponse().get("result") != null) {
+                // The result provided by the API needs to be cast into
+                // the correct type, based upon what types the Apps Script
+                // function returns. Here, the function returns an Apps
+                // Script Object with String keys and values, so must be
+                // cast into a Java Map (folderSet).
+                Map<String, String> folderSet =
+                        (Map<String, String>)(op.getResponse().get("result"));
+
+                for (String id: folderSet.keySet()) {
+                    folderList.add(
+                            String.format("%s (%s)", folderSet.get(id), id));
+                }
+            }
+
+            return folderList;
+        }
+
+        /**
+         * Interpret an error response returned by the API and return a String
+         * summary.
+         *
+         * @param op the Operation returning an error response
+         * @return summary of error response, or null if Operation returned no
+         *     error
+         */
+        private String getScriptError(Operation op) {
+            if (op.getError() == null) {
+                return null;
+            }
+
+            // Extract the first (and only) set of error details and cast as a Map.
+            // The values of this map are the script's 'errorMessage' and
+            // 'errorType', and an array of stack trace elements (which also need to
+            // be cast as Maps).
+            Map<String, Object> detail = op.getError().getDetails().get(0);
+            List<Map<String, Object>> stacktrace =
+                    (List<Map<String, Object>>)detail.get("scriptStackTraceElements");
+
+            StringBuilder sb =
+                    new StringBuilder("\nScript error message: ");
+            sb.append(detail.get("errorMessage"));
+
+            if (stacktrace != null) {
+                // There may not be a stacktrace if the script didn't start
+                // executing.
+                sb.append("\nScript error stacktrace:");
+                for (Map<String, Object> elem : stacktrace) {
+                    sb.append("\n  ");
+                    sb.append(elem.get("function"));
+                    sb.append(":");
+                    sb.append(elem.get("lineNumber"));
+                }
+            }
+            sb.append("\n");
+            return sb.toString();
+        }
+
+
+        @Override
+        protected void onPreExecute() {
+            mOutputText.setText("");
+            mProgress.show();
+        }
+
+        @Override
+        protected void onPostExecute(List<String> output) {
+            mProgress.hide();
+            if (output == null || output.size() == 0) {
+                mOutputText.setText("No results returned.");
+            } else {
+                output.add(0, "Data retrieved using the Google Apps Script Execution API:");
+                mOutputText.setText(TextUtils.join("\n", output));
+
+                //update arrayList
+                arrayList.clear();
+                arrayList.addAll(output);
+                //notify the ListView Adapter that data has changed.
+                Log.d(TAG, Arrays.toString(arrayList.toArray(new String[]{""})));
+                arrayAdapter.notifyDataSetChanged();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mProgress.hide();
+            if (mLastError != null) {
+                if (mLastError instanceof GooglePlayServicesAvailabilityIOException) {
+                    showGooglePlayServicesAvailabilityErrorDialog(
+                            ((GooglePlayServicesAvailabilityIOException) mLastError)
+                                    .getConnectionStatusCode());
+                } else if (mLastError instanceof UserRecoverableAuthIOException) {
+                    startActivityForResult(
+                            ((UserRecoverableAuthIOException) mLastError).getIntent(),
+                            MainActivity2.REQUEST_AUTHORIZATION);
+                } else {
+                    mOutputText.setText("The following error occurred:\n"
+                            + mLastError.getMessage());
+                }
+            } else {
+                mOutputText.setText("Request cancelled.");
+            }
+        }
     }
 }
